ddbcc1ebd542e10b35e45aa67807d0c0757659af
CarissaAmoraj13 <mkcjt0619@gmail.com>
2023-11-24 21:36:18 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+/**
+ ** Increment and Decrement
+ */
+let increment = (id)=>{
+    let selectedItem = id;
+
+    console.log(selectedItem.id);
+}
+
+let decrement = ()=>{
+    let selectedItem = id;
+
+    console.log(selectedItem.id);
+}
+
+
+let update= ()=>{}
+
+
+// Function to navigate to a specific page
+function navigateTo(pageUrl) {
+    window.location.href = pageUrl;
+  }
+  
+
+
+
+  
